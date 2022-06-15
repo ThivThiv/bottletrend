24ec921ab46d60e204cfadcfe35e292fdda2f5ef
Samuel Coizet <messa57fr@gmail.com>
2022-06-15 16:57:18 +0200

    view index batches

--- app/controllers/batches_controller.rb
+++ app/controllers/batches_controller.rb
@@ -1,17 +1,19 @@
 class BatchesController < ApplicationController
-    before_action :set_batch, only: [:show]
+  before_action :set_batch, only: [:show]
+
   def index
     @batches = Batch.all
   end
-  
+
   def show
     @domain = Domain.find(@batch.domain_id)
   end
 
   private
-  
-    def batch_params
-    params.require(:batch).permit(:quantity, :initial_price, :domain_id, :name, :description, :year, :potential, :region)
+
+  def batch_params
+  params.require(:batch).permit(:quantity, :initial_price, :domain_id, :name, :description, :year, :potential, :region)
+  end
 
   def set_batch
     @batch = Batch.find(params[:id])
