5702541dff7de88c25a6ed2ec85d312348f792f9
Thiven Nguyen <thiven.nguyen@kedgebs.com>
2022-06-15 14:30:51 +0200

    Home batch domain done

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   get 'batches/:id', to: 'batches#show', as: 'bottle'
+  get "domains/:id", to:"domains#show", as: "domain"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/batches", to:"batches#index"
   get "/domains", to:"domains#index"
