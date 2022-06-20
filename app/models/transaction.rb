a29232d9b33cae848bac63b1a7a38a07d20b0f3e
Samuel Coizet <messa57fr@gmail.com>
2022-06-20 10:52:14 +0200

    buy bottle new

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -12,12 +12,25 @@ class Transaction < ApplicationRecord
     bottles.each do |bottle|
       transaction = Transaction.new
       transaction.user = user
-      transaction.bottle = bottle
-      if !bottle.transactions.blank?
-        transaction.price = 900 # a calculer en fct des dernières transaction + algo
-      end
+      # added price to transaction si domain sales
+      transaction.price = bottle.batch.initial_price
+      balance = user.balance
+      balance -= transaction.price
+      # if !bottle.transactions.blank?
+      #   transaction.price = 900 # a calculer en fct des dernières transaction + algo
+      # end
       transaction.save
+      user.update(balance: balance)
       transactions << transaction
     end
   end
+
+  # def self.deduct_balance
+  #     user = current_user
+  #     price = transaction.price
+  #     user.balance = user.balance - price
+  #   end
+  # end
+
+
 end
