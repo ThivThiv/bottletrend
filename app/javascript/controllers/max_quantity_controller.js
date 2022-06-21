ae8d4faf8ec09d7d29857960bc43af175ee81b34
cedrick martin <cedrick.martin@kedgebs.com>
2022-06-21 13:00:57 +0200

    on a bien avancé sur la page new transaction

--- app/javascript/controllers/max_quantity_controller.js
+++ app/javascript/controllers/max_quantity_controller.js
@@ -3,15 +3,19 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
 
   static targets = ["input", "alert"]
-  static values = ["domain", "users"]
+  static values = {
+    domain: Number,
+    users: Number
+  }
 
   connect() {
-    console.log(this.domainValue)
+    console.log("hello")
   }
 
   available() {
     const inputValue = Number(this.inputTarget.value)
     const stockValue = Number(this.domainValue) + Number(this.usersValue)
+    console.log(stockValue)
       if (inputValue > stockValue) {
         this.alertTarget.classList.remove('d-none')
       } else {
