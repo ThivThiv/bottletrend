b486c97fbf057257791c35fe0db7dc5dff3ff440
Samuel Coizet <messa57fr@gmail.com>
2022-06-23 10:45:53 +0200

    notif sale

--- app/javascript/controllers/toggle_modal_controller.js
+++ app/javascript/controllers/toggle_modal_controller.js
@@ -2,7 +2,15 @@ import { Controller } from "stimulus"
 
 export default class extends Controller {
 
+  static targets = ["modal"]
+
   connect() {
-    console.log("ok")
+    console.log(this.totoTarget)
+  }
+
+  displayModal () {
+    // this.modalTarget.style.backgroundColor = "green"
+    console.log(this.modalTarget)
+    this.modalTarget.classList.add("active")
   }
 }
